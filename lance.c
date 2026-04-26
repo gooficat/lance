0828286545c3ee65f35aa57034599ac2e3e62cec
Hasan Sabri <hsabri2007@gmail.com>
2026-04-26 21:39:52 +0100

    beta reductions

--- lance.c
+++ lance.c
@@ -1,6 +1,7 @@
 /* Lambda calculus implementation in plain C90*/
 
 #include <ctype.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,6 +28,7 @@ struct Expr
 		{
 			char name;
 			struct Expr *expr;
+			struct Expr *parent;
 		} abs;
 		struct ExprApp
 		{
@@ -52,12 +54,14 @@ void Expr_print(struct Expr *self)
 		Expr_print(self->app.a);
 		Expr_print(self->app.b);
 		putchar(')');
+		break;
 	default:
+		puts("Nil");
 		break;
 	}
 }
 
-const char *Expr_parse(struct Expr *expr, const char *s)
+const char *Expr_parse(struct Expr *expr, const char *s, struct Expr *parent)
 {
 	switch (*s)
 	{
@@ -67,15 +71,16 @@ const char *Expr_parse(struct Expr *expr, const char *s)
 		expr->abs.name = *s;
 		expr->abs.expr = malloc(sizeof *expr->abs.expr);
 		s += 2;
-		s = Expr_parse(expr->abs.expr, s);
+		s = Expr_parse(expr->abs.expr, s, expr);
+		expr->abs.parent = parent;
 		break;
 	case '(':
 		expr->type = EXPR_APP;
 		expr->app.a = malloc(sizeof *expr->app.a);
 		expr->app.b = malloc(sizeof *expr->app.b);
 		++s;
-		s = Expr_parse(expr->app.a, s);
-		s = Expr_parse(expr->app.b, s);
+		s = Expr_parse(expr->app.a, s, parent);
+		s = Expr_parse(expr->app.b, s, parent);
 		++s;
 		break;
 	default:
@@ -131,7 +136,7 @@ struct Expr *read_program(const char *path)
 
 	if (*content != '\0')
 	{
-		content = Expr_parse(expr, content);
+		content = Expr_parse(expr, content, NULL);
 	}
 	else
 	{
@@ -141,14 +146,64 @@ struct Expr *read_program(const char *path)
 	return expr;
 }
 
-struct Expr *interpret(const struct Expr *expr)
+struct Expr *deep_clone(struct Expr *expr)
 {
-	//
+	struct Expr *out = malloc(sizeof *out);
+	out->type = expr->type;
+	switch (expr->type)
+	{
+	case EXPR_VAR:
+		out->var = expr->var;
+		break;
+	case EXPR_ABS:
+		out->abs.name = expr->abs.name;
+		out->abs.parent = expr->abs.parent;
+		out->abs.expr = deep_clone(expr->abs.expr);
+		break;
+	case EXPR_APP:
+		out->app.a = deep_clone(expr->app.a);
+		out->app.b = deep_clone(expr->app.b);
+	default:
+		break;
+	}
+	return out;
+}
+
+struct Expr *substitute(struct Expr *expr, char v, struct Expr *sub)
+{
+	struct Expr *old = expr;
+	switch (expr->type)
+	{
+	case EXPR_ABS:
+		if (expr->abs.name != v)
+			expr->abs.expr = substitute(expr->abs.expr, v, sub);
+		free(old);
+		return expr;
+	case EXPR_APP:
+		substitute(expr->app.a, v, sub);
+		substitute(expr->app.b, v, sub);
+	case EXPR_VAR:
+		if (expr->var == v)
+		{
+			free(old);
+			return deep_clone(sub);
+		}
+	default:
+		return expr;
+	}
+}
+
+struct Expr *beta_reduction(struct Expr *expr)
+{
+	if (expr->type == EXPR_APP)
+		return substitute(expr->app.a->abs.expr, expr->app.a->abs.name, expr->app.b);
+	return expr;
 }
 
 int main(int argc, char **argv)
 {
 	struct Expr *expr;
+	// struct Expr *pool[UINT8_MAX] = {0};
 	if (2 != argc)
 	{
 		fatal("No input file");
@@ -158,6 +213,9 @@ int main(int argc, char **argv)
 
 	expr = read_program(argv[1]);
 
+	Expr_print(expr);
+	putchar('\n');
+	expr = beta_reduction(expr);
 	Expr_print(expr);
 
 	return 0;
